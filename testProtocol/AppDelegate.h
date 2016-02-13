9b5b911bc835f3c653c6b733042dc66b59b1910c
Ben LIU <arkilis@gmail.com>
2016-02-14 06:41:10 +1000

    Initial Commit

---
+++ testProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProtocol
+//
+//  Created by Ben Liu on 14/02/2016.
+//  Copyright © 2016 Ben Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProtocol
+//
+//  Created by Ben Liu on 14/02/2016.
+//  Copyright © 2016 Ben Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
